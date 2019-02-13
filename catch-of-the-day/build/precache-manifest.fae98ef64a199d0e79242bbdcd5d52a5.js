99946f6d6748f715f1646cf1b7c10a848c0cc7ab
Jesús Ángel Pérez-Roca Fernández <yisus82@users.noreply.github.com>
2019-02-13 10:12:04 +0100

    Second try

---
+++ catch-of-the-day/build/precache-manifest.fae98ef64a199d0e79242bbdcd5d52a5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "61d33f47ff579e14f381",
+    "url": "/wesbos-react-beginners/static/css/main.f7e1765d.chunk.css"
+  },
+  {
+    "revision": "61d33f47ff579e14f381",
+    "url": "/wesbos-react-beginners/static/js/main.61d33f47.chunk.js"
+  },
+  {
+    "revision": "661885dc57749fa245a6",
+    "url": "/wesbos-react-beginners/static/js/runtime~main.661885dc.js"
+  },
+  {
+    "revision": "df5d4b122825b5de0c32",
+    "url": "/wesbos-react-beginners/static/js/2.df5d4b12.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/wesbos-react-beginners/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/wesbos-react-beginners/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/wesbos-react-beginners/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/wesbos-react-beginners/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/wesbos-react-beginners/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/wesbos-react-beginners/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/wesbos-react-beginners/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/wesbos-react-beginners/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/wesbos-react-beginners/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "9a8a026b8625289fe5da81e8dcd720a6",
+    "url": "/wesbos-react-beginners/index.html"
+  }
+];
--- catch-of-the-day/build/service-worker.js
+++ catch-of-the-day/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a7539b421fc6090bd938c75337db07c.js"
+  "/wesbos-react-beginners/precache-manifest.fae98ef64a199d0e79242bbdcd5d52a5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/wesbos-react-beginners/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
