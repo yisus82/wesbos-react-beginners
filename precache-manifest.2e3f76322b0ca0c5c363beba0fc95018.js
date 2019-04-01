3c2cbb13993a770a81f3282ba6250d97a3a5b225
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2019-04-02 01:35:12 +0200

    Updates

---
+++ precache-manifest.2e3f76322b0ca0c5c363beba0fc95018.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "803615a4fa5b287ce0ff",
+    "url": "/wesbos-react-beginners/static/css/main.20af04d8.chunk.css"
+  },
+  {
+    "revision": "803615a4fa5b287ce0ff",
+    "url": "/wesbos-react-beginners/static/js/main.261193b5.chunk.js"
+  },
+  {
+    "revision": "16f5a622723be294f075",
+    "url": "/wesbos-react-beginners/static/js/runtime~main.359a76f5.js"
+  },
+  {
+    "revision": "2acbb67744ff638b7cb0",
+    "url": "/wesbos-react-beginners/static/js/2.ae80b2c8.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/wesbos-react-beginners/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/wesbos-react-beginners/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/wesbos-react-beginners/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/wesbos-react-beginners/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/wesbos-react-beginners/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/wesbos-react-beginners/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/wesbos-react-beginners/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/wesbos-react-beginners/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/wesbos-react-beginners/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "f7a741bfdad7ababf9f394b34bcedcbc",
+    "url": "/wesbos-react-beginners/index.html"
+  }
+];
