dc23ace9cf36bbe3ca03d7cae536c2596fa5c4f8
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2019-04-02 14:13:24 +0200

    Added basename in BrowserRouter

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import StorePicker from './StorePicker';
 import App from './App';
 import NotFound from './NotFound';
 
 const Router = () => (
-  <HashRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route exact path="/" component={StorePicker} />
       <Route path="/store/:storeId" component={App} />
       <Route component={NotFound} />
     </Switch>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default Router;
